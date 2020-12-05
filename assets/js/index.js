0bd3aa837e1f1825ac0172d8dea6136cc83cca29
seleshabani <seleshabani4@gmail.com>
2020-12-05 04:33:12 +0100

    first

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+$("#tsparticles").particles().ajax("assets/js/particles_conf.json", function (container) {
+    // container is the particles container where you can play/pause or stop/start.
+    // the container is already started, you don't need to start it manually.
+});
+  
